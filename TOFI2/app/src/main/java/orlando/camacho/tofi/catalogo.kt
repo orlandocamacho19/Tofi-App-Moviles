0007ab6109657e538f3c235e8cba267b0bdac3ea
AngelHdez21 <91582695+AngelHdez21@users.noreply.github.com>
2022-05-16 19:32:37 -0700

    Actualizacion
    Carga las historias de la base de datos

--- TOFI2/app/src/main/java/orlando/camacho/tofi/catalogo.kt
+++ TOFI2/app/src/main/java/orlando/camacho/tofi/catalogo.kt
@@ -1,25 +1,53 @@
 package orlando.camacho.tofi
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import com.google.firebase.firestore.FirebaseFirestore
 
 class catalogo : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_catalogo)
 
         val botonClick: ImageButton = findViewById(R.id.btnClick)
+        val botonMochila: ImageButton = findViewById(R.id.btnMochila)
+        val botonMuseo : ImageButton = findViewById(R.id.btnMuseo)
+        val botonPastelito : ImageButton = findViewById(R.id.btnPastelito)
         val botonInicio: ImageButton = findViewById(R.id.btnInicio)
         val botonBuscar: ImageButton = findViewById(R.id.btnBuscar)
         val botonBiblioteca: ImageButton = findViewById(R.id.btnBiblioteca)
         val botonActividades: ImageButton = findViewById(R.id.btnActividades)
         val botonPerfil: ImageButton = findViewById(R.id.btnPerfil)
 
-
         botonClick.setOnClickListener {
             var intent: Intent = Intent (this, presentacion_libro::class.java)
+            var libroSelec="Click"
+            intent.putExtra("Libro", libroSelec)
+            startActivity(intent)
+        }
+
+        botonMochila.setOnClickListener{
+            var intent: Intent = Intent (this, presentacion_libro::class.java)
+            var libroSelec="Mochila"
+            intent.putExtra("Libro", libroSelec)
+            startActivity(intent)
+        }
+
+        botonMuseo.setOnClickListener{
+            var intent: Intent = Intent (this, presentacion_libro::class.java)
+            var libroSelec="Museo"
+            intent.putExtra("Libro", libroSelec)
+            startActivity(intent)
+        }
+
+        botonPastelito.setOnClickListener{
+            var intent: Intent = Intent (this, presentacion_libro::class.java)
+            var libroSelec="Pastelito"
+            intent.putExtra("Libro", libroSelec)
             startActivity(intent)
         }
 
